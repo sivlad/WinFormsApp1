1e99adba19d4f29c3ee3e035366855c47f30b37a
Admin <sivlad1979@gmail.com>
2026-03-27 20:10:47 +0200

    Add entity AcademyGroup

---
+++ WinFormsApp1/AcademyGroup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace CodeFirst
+{
+    public class AcademyGroup
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
