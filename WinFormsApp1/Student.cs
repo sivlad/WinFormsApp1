b32029fb540b9907308e5edce59dd66d0b330495
Admin <sivlad1979@gmail.com>
2026-04-03 19:50:58 +0300

    Add properties Address and City into entity Student

--- WinFormsApp1/Student.cs
+++ WinFormsApp1/Student.cs
@@ -8,6 +8,8 @@
         public int? Age { get; set; }
         public double? PointAverage { get; set; }
 
+        public string Address { get; set; }
+        public string City { get; set; }
         public virtual AcademyGroup AcademyGroup { get; set; }
     }
 }
